649556176957ebc8dc165efaa0330721a883a37a
Jeevan-Rai <78399976+Jeevan-Rai@users.noreply.github.com>
2022-09-18 10:26:53 +0530

    about us final

--- aboutkvgce copy.php
+++ aboutkvgce copy.php
@@ -654,105 +654,105 @@ include('navbar.php');
 
     <body>
 
-    <div class="container"> 
-	<center>
-		<h2 class="head1">ABOUT <span class="head2">US</span></h2>
-	</center>
-	<hr>
-</div>
+        <div class="container">
+            <center>
+                <h2 class="head1">ABOUT <span class="head2">US</span></h2>
+            </center>
+            <hr>
+        </div>
 
 
-<section id="about">
-	<div class="container">
-		<p>The KVG Dental college & hospital, Kurunjibag, Sullia was established in the year 1991. The first batch of 40 students celebrated their graduation on august1996 and began their carrier as dental professionals. The college was recognized by DCI and RGUHS with 100 admissions in the year 2001 and currently runs post graduate courses in 9 specialties. The college has progressed leaps and bounds in keeping quality dental education imbibed with proficiency and human value. This emporium has reached its zenith with completion of two decades in dental education.</p>
-		<p>The under graduate and postgraduate teaching programme are supported by an outstanding infrastructure which includes state of art, esthetically designed individual departments and well stocked library with lively shape and outlook.</p>
-		<p>The highly qualified faculties with rich experience of clinical and administrative skills are backbone for this esteemed institution. The management and faculty not only guide the students in dental education but also strive to develop student’s extracurricular activities and talents with sound morals, value.</p>
-	</div>
-</section>
-<div class="clearfix"></div>
-<br><br><br>
+        <section id="about">
+            <div class="container">
+                <p>The KVG Dental college & hospital, Kurunjibag, Sullia was established in the year 1991. The first batch of 40 students celebrated their graduation on august1996 and began their carrier as dental professionals. The college was recognized by DCI and RGUHS with 100 admissions in the year 2001 and currently runs post graduate courses in 9 specialties. The college has progressed leaps and bounds in keeping quality dental education imbibed with proficiency and human value. This emporium has reached its zenith with completion of two decades in dental education.</p>
+                <p>The under graduate and postgraduate teaching programme are supported by an outstanding infrastructure which includes state of art, esthetically designed individual departments and well stocked library with lively shape and outlook.</p>
+                <p>The highly qualified faculties with rich experience of clinical and administrative skills are backbone for this esteemed institution. The management and faculty not only guide the students in dental education but also strive to develop student’s extracurricular activities and talents with sound morals, value.</p>
+            </div>
+        </section>
+        <div class="clearfix"></div>
+        <br><br><br>
 
 
-    <section id="mission">
-	<div class="container">
-		<div class="row">
+        <section id="mission">
+            <div class="container">
+                <div class="row">
 
-			<div class="col-sm-5">
-				<div class="row box" style="margin: auto !IMPORTANT;">
-					<div class="col-sm-3">
-						<center>
-							<i class="fa fa-eye" aria-hidden="true"></i>
-						</center>
-					</div>
-					<div class="col-sm-9">
-						<h3>Vision</h3>
-						<p>To be an Institution of excellence in dental education and to provide quality dental healthcare to rural population.</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-7">
-				<div class="row box" style="margin: auto !IMPORTANT;">
-					<div class="col-sm-3">
-						<center>
-							<i class="fa fa-rocket" aria-hidden="true"></i>
-						</center>
-					</div>
-					<div class="col-sm-9">
-						<h3>Mission</h3>
-						<ul class="svc">
-							<li>Working together and effectively for curricular development, teaching learning and evaluation process.</li>
-							<li>Cultivating an environment to promote  research innovations</li>
-							<li>Focusing on rural public health and community services.</li>
-						</ul>
+                    <div class="col-sm-5">
+                        <div class="row box" style="margin: auto !IMPORTANT;">
+                            <div class="col-sm-3">
+                                <center>
+                                    <i class="fa fa-eye" aria-hidden="true"></i>
+                                </center>
+                            </div>
+                            <div class="col-sm-9">
+                                <h3>Vision</h3>
+                                <p>To be an Institution of excellence in dental education and to provide quality dental healthcare to rural population.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-7">
+                        <div class="row box" style="margin: auto !IMPORTANT;">
+                            <div class="col-sm-3">
+                                <center>
+                                    <i class="fa fa-rocket" aria-hidden="true"></i>
+                                </center>
+                            </div>
+                            <div class="col-sm-9">
+                                <h3>Mission</h3>
+                                <ul class="svc">
+                                    <li>Working together and effectively for curricular development, teaching learning and evaluation process.</li>
+                                    <li>Cultivating an environment to promote research innovations</li>
+                                    <li>Focusing on rural public health and community services.</li>
+                                </ul>
 
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-	<style type="text/css">
-		#mission i{
-			font-size: 80px;
-			color: #23c1d7;
-			text-shadow: 1px 1px 5px #3a3a3a;
-			text-shadow: 20px 16px 8px #cccccc;
-			margin-top: 15px;
-		}
+            <style type="text/css">
+                #mission i {
+                    font-size: 80px;
+                    color: #23c1d7;
+                    text-shadow: 1px 1px 5px #3a3a3a;
+                    text-shadow: 20px 16px 8px #cccccc;
+                    margin-top: 15px;
+                }
 
-		#mission .box{
-			padding: 10px;
-			box-shadow: -1px 5px 13px #cecece;
-			min-height: 240px;
-		}
-	</style>
-</section>
+                #mission .box {
+                    padding: 10px;
+                    box-shadow: -1px 5px 13px #cecece;
+                    min-height: 240px;
+                }
+            </style>
+        </section>
 
 
-<div class="clearfix"></div>
-<br><br><br>
+        <div class="clearfix"></div>
+        <br><br><br>
 
-<section id="founder" class="bshadow" ><br><br>
-	<div class="container">
+        <section id="founder" class="bshadow"><br><br>
+            <div class="container">
 
-		<div class="row">
-			<div class="col-sm-12">
-				<h2 class="head1 pull-right"><span class="head2">FOUNDER</span> PRESIDENT</h2>
-				<div class="clearfix"></div><br>
-			</div>
-			<div class="clearfix"></div><br>
-			<div class="col-sm-4">
-				<img src="images/about/founder.png" class="img-responsive">
-			</div>
-			<div class="col-sm-8">
-				<p>Late Dr. Kurunji Venkatramana Gowda, popularly known as ‘KVG’ was a true Gandhian in spirit and action and has devoted his entire life in changing the destinies of the large rural mass of Sullia taluk. Born in 1928, in an agricultural family, he is a BHAGEERATHA who has brought JNANA GANGA to this small town and made Sullia a VIDYA KASHI. Powered by the visionary leadership of Late Dr. KVG, the academy of Liberal Education today is a registered body with more than fifty educational Institutions. He is the source of inspiration to all. He is a man of many dimensions. He is a model agriculturist, an educationist, a patron of fine arts and cultural endeavors, a social reformer par excellence and a philanthropist of great stature. All these have made him a living legend. Amashree Kurunji Venkatramana Gowda a man who dares to dream and works equally hard to make this dreams come true. An architect of Modern Sullia and a visionary amer with a vision “son of the soil”.Late Sri Kurunji Venkatramana Gowda’s consistent endeavors have turned his dream into reality. Son of the soil that he is chose Sullia and changed it into an educational landmark</p>
-			</div>
-		</div>
-	</div>
-	<div class="clearfix"></div><br><br>
-</section>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h2 class="head1 pull-right"><span class="head2">FOUNDER</span> PRESIDENT</h2>
+                        <div class="clearfix"></div><br>
+                    </div>
+                    <div class="clearfix"></div><br>
+                    <div class="col-sm-4">
+                        <img src="images/about/founder.png" class="img-responsive">
+                    </div>
+                    <div class="col-sm-8">
+                        <p>Late Dr. Kurunji Venkatramana Gowda, popularly known as ‘KVG’ was a true Gandhian in spirit and action and has devoted his entire life in changing the destinies of the large rural mass of Sullia taluk. Born in 1928, in an agricultural family, he is a BHAGEERATHA who has brought JNANA GANGA to this small town and made Sullia a VIDYA KASHI. Powered by the visionary leadership of Late Dr. KVG, the academy of Liberal Education today is a registered body with more than fifty educational Institutions. He is the source of inspiration to all. He is a man of many dimensions. He is a model agriculturist, an educationist, a patron of fine arts and cultural endeavors, a social reformer par excellence and a philanthropist of great stature. All these have made him a living legend. Amashree Kurunji Venkatramana Gowda a man who dares to dream and works equally hard to make this dreams come true. An architect of Modern Sullia and a visionary amer with a vision “son of the soil”.Late Sri Kurunji Venkatramana Gowda’s consistent endeavors have turned his dream into reality. Son of the soil that he is chose Sullia and changed it into an educational landmark</p>
+                    </div>
+                </div>
+            </div>
+            <div class="clearfix"></div><br><br>
+        </section>
 
-<div class="clearfix"></div><br>
+        <div class="clearfix"></div><br>
 
 
         <main id="vm">
@@ -1824,7 +1824,7 @@ include('navbar.php');
                                 <h3 class="subheadWhite head" style="color:#3A3B3C;padding:0%;"> Founder President<span></span></h3>
                                 <div class="card7-title fw-7" style="color:black;font-size:16px;text-align:center;">LATE Dr. KURUNJI VENKATRAMANA GOWDA</div>
                                 <div class="card7-text fw-5" style="color:#3A3B3C;">
-                                    <p style="text-align: justify;">Guided by the basic philosophy “JNANAM SARVATHRA SADHANAM” and with an aim of building Sullia as a world class learning centre<span id="dots">...</span><span id="more">, Dr. KURUNJI VENKATRAMANA GOWDA, Founder President, Academy of Liberal Education has converted tiny village into a modern learning center of the country. Powered by the visionary leadership today AOLE® is a conglomerate with more than fifty educational institutions. Born as, one of the millions became one in millions. He is BHAGEERATHA who brought JNANA GANGA to this small town and made Sullia a VIDYA KASHI. <br> &nbsp;&nbsp;&nbsp;&nbsp;Dr. KVG is a man of many dimensions. He is an Educationist, a social reformer, a philosopher, a great preacher an able administrator. Above all a great philanthropist. All these have made him a Living Legend. He is the source of inspiration to all of us. His ideals and way of working are to be imbibed in everybody’s life. He has a dream of making our country modern and strong by spreading intellectual spirituality. We at KVG College of Engineering share his dream of making our BHARATH MAHAAN.</span></p>
+                                    <p style="text-align: justify;">LATE Dr. KURUNJI VENKATRAMANA GOWDA Guided by the basic philosophy “JNANAM SARVATHRA SADHANAM” and with an aim of building Sullia as a world class learning centre<span id="dots">...</span><span id="more">, Dr. KURUNJI VENKATRAMANA GOWDA, Founder President, Academy of Liberal Education has converted tiny village into a modern learning center of the country. Powered by the visionary leadership today AOLE® is a conglomerate with more than fifty educational institutions. Born as, one of the millions became one in millions. He is BHAGEERATHA who brought JNANA GANGA to this small town and made Sullia a VIDYA KASHI. <br> &nbsp;&nbsp;&nbsp;&nbsp;Dr. KVG is a man of many dimensions. He is an Educationist, a social reformer, a philosopher, a great preacher an able administrator. Above all a great philanthropist. All these have made him a Living Legend. He is the source of inspiration to all of us. His ideals and way of working are to be imbibed in everybody’s life. He has a dream of making our country modern and strong by spreading intellectual spirituality. We at KVG College of Engineering share his dream of making our BHARATH MAHAAN.</span></p>
                                     <button onclick="myFunction()" id="myBtn"><strong> Read more</strong></button>
                                 </div>
 
